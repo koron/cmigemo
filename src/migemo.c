f5904e66384813c9a70b14836428552504e4cba7
koron <none@none>
2001-08-07 10:55:42 +0000

    Initial revision
    --HG--
    extra : convert_revision : svn%3A1dcf550d-b01c-dc11-aeac-0020ed62593b/trunk%4023

---
+++ src/migemo.c
@@ -0,0 +1,220 @@
+/* vim:set ts=8 sts=4 sw=4 tw=0 */
+/*
+ * migemo.c -
+ *
+ * Written By:  Muraoka Taro <koron@tka.att.ne.jp>
+ * Last Change: 07-Aug-2001.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "wordbuf.h"
+#include "wordlist.h"
+#include "mnode.h"
+#include "rxgen.h"
+#include "romaji.h"
+#include "filename.h"
+#include "migemo.h"
+
+/* migemoオブジェクト */
+struct _migemo
+{
+    mnode* node;
+    romaji* roma2hira;
+    romaji* hira2kata;
+};
+
+/*
+ * migemo interfaces
+ */
+
+    migemo*
+migemo_open(char* dict)
+{
+    migemo *obj = NULL;
+    FILE *fp = NULL;
+
+    if (dict
+	    && (fp = fopen(dict, "rt")) /* 辞書ファイルの存在確認 */
+	    && (obj = (migemo*)malloc(sizeof(migemo))))
+    {
+	char dir[_MAX_PATH], roma_dict[_MAX_PATH], kata_dict[_MAX_PATH];
+
+	obj->node = mnode_open(fp);
+	obj->roma2hira = romaji_open();
+	obj->hira2kata = romaji_open();
+	if (!obj->node || !obj->roma2hira || !obj->hira2kata)
+	{
+	    migemo_close(obj);
+	    obj = NULL;
+	}
+
+	filename_directory(dir, dict);
+	/*printf("dir=\"%s\"\n", dir);*/
+	strcpy(roma_dict, strlen(dir) ? dir : ".");
+	strcpy(kata_dict, strlen(dir) ? dir : ".");
+	strcat(roma_dict, "/romaji.dat");
+	strcat(kata_dict, "/hira2kata.dat");
+	romaji_load(obj->roma2hira, roma_dict);
+	romaji_load(obj->hira2kata, kata_dict);
+    }
+
+    if (fp)
+	fclose(fp); /*  辞書ファイルクローズ */
+    return obj;
+}
+
+    void
+migemo_close(migemo* obj)
+{
+    if (obj)
+    {
+	if (obj->hira2kata)
+	    romaji_close(obj->hira2kata);
+	if (obj->roma2hira)
+	    romaji_close(obj->roma2hira);
+	if (obj->node)
+	    mnode_close(obj->node);
+	free(obj);
+    }
+}
+
+#if 0
+    void
+migemo_query_proc(mnode* p, void* data)
+{
+    wordbuf *buf = (wordbuf*)data;
+    wordlist *list = p->list;
+
+    for (; list; list = list->next)
+    {
+	if (wordbuf_last(buf) > 0)
+	    wordbuf_add(buf, ' ');
+	wordbuf_cat(buf, list->ptr);
+    }
+}
+
+    unsigned char*
+migemo_query(migemo* p, unsigned char* query)
+{
+    unsigned char* answer = NULL;
+    mnode *pnode;
+
+    /*printf("migemo_query(p=%p, query=\"%s\")\n", p, query);*/
+    if (pnode = mnode_query(p->node, query))
+    {
+	wordbuf *pwordbuf;
+
+	pwordbuf = wordbuf_open();
+	/* バッファを用意して再帰でバッファに書き込ませる */
+	mnode_traverse(pnode, migemo_query_proc, pwordbuf);
+	answer = strdup(wordbuf_get(pwordbuf));
+	wordbuf_close(pwordbuf);
+    }
+    /*printf("  pnode=%p, answer=%p\n", query, pnode, answer);*/
+
+    return answer;
+}
+
+    void
+migemo_release(migemo* p, unsigned char* string)
+{
+    free(string);
+}
+#else
+/*
+ * query version 2
+ */
+
+    static void
+migemo_query_proc(mnode* p, void* data)
+{
+    rxgen *rx = (rxgen*)data;
+    wordlist *list = p->list;
+
+    for (; list; list = list->next)
+	rxgen_add(rx, list->ptr);
+}
+
+/*
+ * ローマ字変換が不完全だった時に、[aiueon]を補って変換してみる。
+ */
+    static void
+migemo_query_stub(migemo* object, rxgen* rx, unsigned char* query)
+{
+    int len;
+    char *buf;
+    unsigned char candidate[] = "aiueon", *ptr;
+
+    if (!(len = strlen(query)))
+	return;
+    if (!(buf = malloc(len + 2))) /* NULと拡張文字用 */
+	return;
+    strcpy(buf, query);
+    buf[len + 1] = '\0';
+
+    for (ptr = candidate; *ptr; ++ptr)
+    {
+	unsigned char *stop, *hira, *kata;
+
+	buf[len] = *ptr;
+	hira = romaji_convert(object->roma2hira, buf, &stop);
+
+	if (!stop)
+	{
+	    rxgen_add(rx, hira);
+	    /* 片仮名文字列を生成し候補に加える */
+	    kata = romaji_convert(object->hira2kata, hira, NULL);
+	    rxgen_add(rx, kata);
+	    romaji_release(object->hira2kata, kata);
+	}
+	romaji_release(object->roma2hira, hira);
+    }
+}
+
+    unsigned char*
+migemo_query(migemo* object, unsigned char* query)
+{
+    unsigned char* answer = NULL;
+    mnode *pnode;
+    rxgen *rx = rxgen_open();
+
+    if (rx = rxgen_open())
+    {
+	unsigned char *stop, *hira, *kata;
+
+	rxgen_add(rx, query);
+
+	/* 平仮名文字列を生成し候補に加える */
+	hira = romaji_convert(object->roma2hira, query, &stop);
+	if (!stop)
+	{
+	    rxgen_add(rx, hira);
+	    /* 片仮名文字列を生成し候補に加える */
+	    kata = romaji_convert(object->hira2kata, hira, NULL);
+	    rxgen_add(rx, kata);
+	    romaji_release(object->hira2kata, kata);
+	}
+	else
+	    migemo_query_stub(object, rx, query);
+	romaji_release(object->roma2hira, hira);
+
+	/* バッファを用意して再帰でバッファに書き込ませる */
+	if (pnode = mnode_query(object->node, query))
+	    mnode_traverse(pnode, migemo_query_proc, rx);
+
+	answer = rxgen_generate(rx);
+	rxgen_close(rx);
+    }
+
+    return answer;
+}
+
+    void
+migemo_release(migemo* p, unsigned char* string)
+{
+    rxgen_release(NULL, string);
+}
+#endif
---
+++ src/wordbuf.c
@@ -0,0 +1,126 @@
+/* vim:set ts=8 sts=4 sw=4 tw=0 */
+/*
+ * wordbuf.h -
+ *
+ * Written By:  Muraoka Taro <koron@tka.att.ne.jp>
+ * Last Change: 07-Aug-2001.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "wordbuf.h"
+
+#define WORDLEN_DEF 64
+
+extern int n_wordbuf_open = 0;
+extern int n_wordbuf_close = 0;
+
+struct _wordbuf
+{
+    int len;
+    unsigned char* buf;
+    int last;
+};
+
+/* function pre-declaration */
+static int wordbuf_extend(wordbuf* p, int len);
+
+    wordbuf*
+wordbuf_open()
+{
+    wordbuf *p = (wordbuf*)malloc(sizeof(wordbuf));
+
+    if (p)
+    {
+	++n_wordbuf_open;
+	p->len = WORDLEN_DEF;
+	p->buf = (unsigned char*)malloc(p->len);
+	p->buf[0] = '\0';
+	p->last = 0;
+    }
+    return p;
+}
+
+    void
+wordbuf_close(wordbuf* p)
+{
+    if (p)
+    {
+	++n_wordbuf_close;
+	free(p->buf);
+	free(p);
+    }
+}
+
+    void
+wordbuf_reset(wordbuf* p)
+{
+    p->last = 0;
+    p->buf[0] = '\0';
+}
+
+/*
+ * wordbuf_extend(wordbuf* p, int len);
+ *	バッファの伸長。エラー時には0が帰る。
+ *	高速化のために伸ばすべきかは呼出側で判断する。
+ */
+    static int
+wordbuf_extend(wordbuf* p, int len)
+{
+    int newlen = p->len * 2;
+    unsigned char *newbuf;
+
+    while (len > newlen)
+	newlen *= 2;
+    if (!(newbuf = (unsigned char*)realloc(p->buf, newlen)))
+    {
+	/*fprintf(stderr, "wordbuf_add(): failed to extend buffer\n");*/
+	return 0;
+    }
+    p->len = newlen;
+    p->buf = newbuf;
+
+    return len;
+}
+
+    int
+wordbuf_last(wordbuf* p)
+{
+    return p->last;
+}
+
+    int
+wordbuf_add(wordbuf* p, unsigned char ch)
+{
+    int newlen = p->last + 2;
+
+    if (newlen > p->len && !wordbuf_extend(p, newlen))
+	return 0;
+    p->buf[p->last++]	= ch;
+    p->buf[p->last  ]	= '\0';
+    return p->last;
+}
+
+    int
+wordbuf_cat(wordbuf* p, unsigned char* sz)
+{
+    int len;
+
+    if (sz && (len = strlen(sz)) > 0)
+    {
+	int newlen = p->last + len + 1;
+
+	if (newlen > p->len && !wordbuf_extend(p, newlen))
+	    return 0;
+	memcpy(&p->buf[p->last], sz, len + 1);
+	p->last = p->last + len;
+    }
+    return p->last;
+}
+
+    unsigned char*
+wordbuf_get(wordbuf* p)
+{
+    return p->buf;
+}
