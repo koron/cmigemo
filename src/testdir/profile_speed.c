a0e1f608ecae9d3e20c89a6c0eb7b1229cacc450
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-03-08 12:02:01 +0900

    improve profiler

--- src/testdir/profile_speed.c
+++ src/testdir/profile_speed.c
@@ -1,19 +1,21 @@
-/* vim:set ts=8 sts=4 sw=4 tw=0: */
+/* vim:set ts=8 sts=4 sw=4 tw=0 et: */
 /*
- * profile_speed.c - スピード計測
+ * profile_speed.c - Query speed profiler.
  *
- * Written By:  MURAOKA Taro <koron.kaoriya@gmail.com>
- * Last Change: 20-Jun-2004.
+ * Author:  MURAOKA Taro <koron.kaoriya@gmail.com>
  */
 
 #define NUM_TRIAL 10
 
 #include <stdio.h>
+#include <time.h>
 #include "migemo.h"
 
 #ifndef DICTDIR
 # define DICTDIR "../dict"
 #endif
+
+#define CLOCK2SEC(t)    ((double)(t) / (double)CLOCKS_PER_SEC)
 
     int
 main(int argc, char** argv)
@@ -22,25 +24,35 @@ main(int argc, char** argv)
     char* ans;
     char key[2] = { '\0', '\0' };
     int i;
+    clock_t clock_load = 0, clock_query = 0, clock_tmp = 0;
 
     printf("Start\n");
+    clock_tmp = clock();
     pmig = migemo_open(DICTDIR "/migemo-dict");
+    clock_load = clock() - clock_tmp;
     printf("Loaded\n");
     if (pmig != NULL)
     {
-	for (i = 0; i < NUM_TRIAL; ++i)
-	{
-	    printf("[%d] Progress... ", i);
-	    for (key[0] = 'a'; key[0] <= 'z'; ++key[0])
-	    {
-		printf("%s", key);
-		fflush(stdout);
-		ans = migemo_query(pmig, key);
-		migemo_release(pmig, ans);
-	    }
-	    printf("\n");
-	}
-	migemo_close(pmig);
+        clock_query = 0;
+        for (i = 0; i < NUM_TRIAL; ++i)
+        {
+            printf("[%d] Progress... ", i);
+            for (key[0] = 'a'; key[0] <= 'z'; ++key[0])
+            {
+                printf("%s", key);
+                fflush(stdout);
+                clock_tmp = clock();
+                ans = migemo_query(pmig, key);
+                migemo_release(pmig, ans);
+                clock_query += clock() - clock_tmp;
+            }
+            printf("\n");
+        }
+        migemo_close(pmig);
     }
+    printf("\n");
+    printf("Results:\n");
+    printf("  load: %.3f secs\n", CLOCK2SEC(clock_load));
+    printf("  query: %.3f secs\n", CLOCK2SEC(clock_query));
     return 0;
 }
